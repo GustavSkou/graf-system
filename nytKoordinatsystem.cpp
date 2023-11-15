c95ce586409ded6539cbf01c64da9a4d2b615aab
GustavSkou <55804968+GustavSkou@users.noreply.github.com>
2023-11-15 13:59:13 +0100

    1.1
    reworked "sortKoordinatsystem", to remove dependens on a second vector

--- nytKoordinatsystem.cpp
+++ nytKoordinatsystem.cpp
@@ -17,8 +17,6 @@ class tile {
 };
 
 vector<tile> tiles;                             //vector(tiles) der skal indeholde objekterne fra tile classen
-vector<tile> koordtiles;
-
 
 void genKoordinatsystem(int size) {
 
@@ -27,7 +25,7 @@ void genKoordinatsystem(int size) {
         for(float x = 0; x < size; x=x+0.5) {
             float xkoordinat = x;
             float ykoordinat = y;
-            string tegn;
+            string tegn = " . ";
 
             tile flise(xkoordinat, ykoordinat, tegn);
             tiles.push_back(flise);
@@ -37,49 +35,50 @@ void genKoordinatsystem(int size) {
 
 void sortKoordinatsystem(int size) {
 
-    for (float y = size; y >= 0; y=y-0.5) {
+    float x = 0;
+    float y = size - 0.5;
 
-        for (float x = 0; x < size; x=x+0.5) {
+    for (int n = 0; n < tiles.size(); n++) {                        //swap nr 1
+        
+        for (int i = 0; i < tiles.size(); i++) {                    //swap nr 2
+            
+            if (tiles[i].xAksen == x && tiles[i].yAksen == y) {
 
-            for (int n = 0; n < tiles.size(); n++) {
+                swap (tiles[n], tiles[i] );
 
-                if (tiles[n].yAksen == y && tiles[n].xAksen == x) {
-
-                    float xkoordinat = x;
-                    float ykoordinat = y;
-                    string tegn = " . ";
-
-                    tile sortflise(xkoordinat, ykoordinat, tegn);
-                    koordtiles.push_back(sortflise);
-                    break;
+                x = x + 0.5;    
+                
+                if (x == size) {                                    
+                    x = 0;
+                    y = y - 0.5;    
                 }
+
+                break;
             }
         }
     }
-}   
+}  
 
 void streamKoordinatsystem(const int size) {                            // update koordinatsystem
     
-    cout << "\n \n \n \n \n \n \n \n \n \n \n \n \n \n \n";
-    
-    for (int n = 0; n < koordtiles.size(); n++) {
+    for (int n = 0; n < tiles.size(); n++) {
 
-        if (koordtiles[n].xAksen == size-0.5) {
+        if (tiles[n].xAksen == size-0.5) {
             
-            //cout << "("<< koordtiles[n].xAksen << "," << koordtiles[n].yAksen << ")\n" ;
-            cout << koordtiles[n].tegn << "\n";
+            //cout << "("<< tiles[n].xAksen << "," << tiles[n].yAksen << ")\n" ;
+            cout << tiles[n].tegn << "\n";
         }
         else {
         
-            //cout << "("<< koordtiles[n].xAksen << "," << koordtiles[n].yAksen << ")" ;
-            cout << koordtiles[n].tegn;
+            //cout << "("<< tiles[n].xAksen << "," << tiles[n].yAksen << ")" ;
+            cout << tiles[n].tegn;
         }
     }
 }
 
 void grafTegner(int size) {
-    for (int n = 0; n < koordtiles.size(); n++) {
-        float x = koordtiles[n].xAksen;
+    for (int n = 0; n < tiles.size(); n++) {
+        float x = tiles[n].xAksen;
 
         float y = sin(x/2)+3; 
 
@@ -88,9 +87,9 @@ void grafTegner(int size) {
         y = (0.25 <= yDeci && yDeci < 0.75) ? y = (y-yDeci) + (0.5 - yDeci + yDeci) :
         y = (0.25 > yDeci)                  ? floor(y): 
         y = ceil(y);
-        cout << y << "\n";
+        //cout << y << "\n";
 
-        koordtiles[n].tegn = (koordtiles[n].yAksen == y) ? koordtiles[n].tegn = " X " : koordtiles[n].tegn = koordtiles[n].tegn;    
+        tiles[n].tegn = (tiles[n].yAksen == y) ? tiles[n].tegn = " X " : tiles[n].tegn = tiles[n].tegn;    
     } 
 }
 
